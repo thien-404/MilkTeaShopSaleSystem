3679170813150da0715f142f1935a78c9347b1d1
Thinh <thinhndse184492@fpt.edu.vn>
2024-11-15 09:44:58 +0700

    login

---
+++ MilkTeaShopSalesSystem/MilkTeaShopSaleSystem.BLL/Services/UserService.cs
@@ -0,0 +1,20 @@
+﻿using MilkTeaShopSaleSystem.DAL.Models;
+using MilkTeaShopSaleSystem.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkTeaShopSaleSystem.BLL.Services
+{
+    public class UserService
+    {
+        private UserRepository _repo = new();
+
+        public User GetAccount(string e, string p)
+        {
+            return _repo.GetOne(e, p);
+        }
+    }
+}
---
+++ MilkTeaShopSalesSystem/MilkTeaShopSaleSystem.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using MilkTeaShopSaleSystem.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkTeaShopSaleSystem.DAL.Repositories
+{
+    public class UserRepository
+    {
+        private MilkTeaShopSaleDbContext _context;
+
+        public User GetOne(string e, string p)
+        {
+            _context = new();
+            return _context.Users.FirstOrDefault(x => x.Email.ToLower() == e && x.Password == p);
+        }
+    }
+}
